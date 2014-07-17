08ddb0dd95f186c24b482e00ff7ccd721a3ef271
ivy <597055914@qq.com>
2014-07-17 12:18:16 +0800

    更新项目显示页面UI及相关后台功能

--- item.php
+++ item.php
@@ -35,6 +35,17 @@ $uid=test_input($_GET['uid']);
 
 if($_SESSION['user-login-id']==1){
 
+	if($_POST['del-all-item']=="del-all-item"){
+
+		if(delsvndir($uid)){
+
+		}
+		else{
+			echo '删除失败';
+		}
+	}
+
+
 ?>
 
 <div class="overitem">
@@ -57,6 +68,9 @@ if(strlen($itemname)!=0 and strlen($uid)!=0){
 	}
 	else{echo $itemname.'project';}
 }
+else{
+	header("location:index.php");
+}
 
 ?>
    
@@ -65,6 +79,7 @@ if(strlen($itemname)!=0 and strlen($uid)!=0){
    <table class="table" id="table-size">
 
     <tr>
+    <th><input type="checkbox" value=""></th>
     <th>文件</th>
     <th>详细</th>
     <th>时间</th>
@@ -77,6 +92,7 @@ if(strlen($itemname)!=0 and strlen($uid)!=0){
    for($i = 2;$i<=$a-1;$i++){
    	if($conlist[$i]!="prosetting.afg" and $conlist[$i]!="readme"){
    	   	echo '<tr>
+   	<td><input type="checkbox" value=""></td>   	
     <td>'.iconv('gbk','utf-8',$conlist[$i]).'</td>
     <td>'.$des.'</td>
     <td>1分钟以前</td>
@@ -101,6 +117,52 @@ if(strlen($itemname)!=0 and strlen($uid)!=0){
 
 </div>
 
+<div class="item-left">
+<div class="panel panel-default">
+  <div class="panel-heading">
+    <h3 class="panel-title">管理</h3>
+  </div>
+  <div class="panel-body">
+
+<?php
+ $url='item.php?name='.$itemname.'&uid='.$uid;
+?>
+<form name="operation" action="<?php echo $url; ?>" method="post">
+
+   <div class="navbar-form">
+         <select class="form-control input-sm">
+              <option value="operate">批量操作</option>
+              <option value="delete">删除</option>
+         </select>
+         <button type="submit" class="btn btn-default btn-sm" id="speace">应用</button>
+         <button type="button" class="btn btn-danger btn-sm" id="deleteallitem"  data-toggle="modal" data-target="#myModal">删除项目</button>
+    </div>
+
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+        <h4 class="modal-title" id="myModalLabel">确认删除</h4>
+      </div>
+      <div class="modal-body">
+        您所进行的操作无法撤销,确认删除吗?
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">取消</button>
+        <button type="submit" class="btn btn-primary" name="del-all-item" value="del-all-item">确认删除</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+</form>
+
+
+  </div>
+</div>
+</div>
+
 <div class="item-right">
 
 <div class="panel panel-default">
@@ -108,7 +170,8 @@ if(strlen($itemname)!=0 and strlen($uid)!=0){
     <h3 class="panel-title">未定栏</h3>
   </div>
   <div class="panel-body">
-    未定数据
+    
+
   </div>
 </div>
 
