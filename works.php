14337de41240540ea5b70b207038f07d7b283780
ivy <597055914@qq.com>
2014-07-29 17:22:24 +0800

    增加前台win8卡片式风格

--- works.php
+++ works.php
@@ -63,12 +63,42 @@ foreach ($reco->newitemmarks as $key => $marks) {
 }
 }
 
+function CataIslegal($cata){
+	switch ($cata) {
+		case "music":
+			return true;
+			break;
+		case "code":
+		    return true;
+		    break;
+		case "script":
+		    return true;
+		    break;
+		case "enactment":
+		    return true;
+		    break;
+		case "dubbing":
+		    return true;
+		    break;
+		case "storyboard":
+		    return true;
+		    break;
+		default:
+			return false;
+			break;
+	}
+}
+
+$cata_=test_input($_GET['cata']);
+
+if(CataIslegal($cata_)==false){
+
 ?>
 
 <div class="acg-works-left">
 
 <div class="acg-script">
-<p id="e-col-first"><span class="glyphicon glyphicon-book" id="col-icon"></span> 脚本</p>
+<p id="e-col-first"><span class="glyphicon glyphicon-book" id="col-icon"></span> <a href="works.php?cata=script">脚本</a></p>
 
 <?php
 PrintProRecommended(1);
@@ -77,35 +107,35 @@ PrintProRecommended(1);
 </div>
 
 <div class="acg-storyboard">
-<p id="e-col"><span class="glyphicon glyphicon-facetime-video" id="col-icon"></span> 分镜</p>
+<p id="e-col"><span class="glyphicon glyphicon-facetime-video" id="col-icon"></span> <a href="works.php?cata=storyboard">分镜</a></p>
 <?php
 PrintProRecommended(2);
 ?>
 </div>
 
 <div class="acg-people">
-<p id="e-col"><span class="glyphicon glyphicon-globe" id="col-icon"></span> 设定</p>
+<p id="e-col"><span class="glyphicon glyphicon-globe" id="col-icon"></span> <a href="works.php?cata=enactment">设定</a></p>
 <?php
 PrintProRecommended(3);
 ?>
 </div>
 
 <div class="acg-code">
-<p id="e-col"><span class="glyphicon glyphicon-file" id="col-icon"></span> 代码</p>
+<p id="e-col"><span class="glyphicon glyphicon-file" id="col-icon"></span> <a href="works.php?cata=code">代码</a></p>
 <?php
 PrintProRecommended(4);
 ?>
 </div>
 
 <div class="acg-dubbing">
-<p id="e-col"><span class="glyphicon glyphicon-headphones" id="col-icon"></span> 配音</p>
+<p id="e-col"><span class="glyphicon glyphicon-headphones" id="col-icon"></span> <a href="works.php?cata=dubbing">配音</a></p>
 <?php
 PrintProRecommended(5);
 ?>
 </div>
 
 <div class="acg-dubbing">
-<p id="e-col"><span class="glyphicon glyphicon-music" id="col-icon"></span> 音乐</p>
+<p id="e-col"><span class="glyphicon glyphicon-music" id="col-icon"></span> <a href="works.php?cata=music">音乐</a></p>
 <?php
 PrintProRecommended(6);
 ?>
@@ -163,5 +193,99 @@ if($itemmem!=0){
 </div>
 
 </div>
+<?php
+}else{
+	switch ($cata_) {
+		case "music":
+			
+			break;
+		case "code":
+		    
+		    break;
+		case "script":
+?>
+<div class="overitem">
+
+<div class="newest-submit">
+<p id="e-col-newest"><span class="glyphicon glyphicon-repeat" id="col-icon"></span> <a href="works.php?cata=music">最新投稿</a></p>
+
+  <div class="page-col"><a href=""><span class="glyphicon glyphicon-circle-arrow-right"></span></a></div>
+  <div class="page-col"><a href=""><span class="glyphicon glyphicon-circle-arrow-left"></span></a></div>  
+
+  <div class="the-top-three">
+
+    <div class="the-top-three-content">
+    <span><a href="">SBSengine</a></span>
+    </div>
+
+    <div class="the-top-three-footer">
+    <a href="">ivy</a>
+    </div>
+
+  </div>
+
+  <div class="the-top-three">
+
+    <div class="the-top-three-content">
+    <span><a href="">SBSengine</a></span>
+    </div>
+
+    <div class="the-top-three-footer">
+    <a href="">ivy</a>
+    </div>
+
+  </div>
+
+  <div class="the-top-three">
+
+    <div class="the-top-three-content">
+    <span><a href="">SBSengine</a></span>
+    </div>
+
+    <div class="the-top-three-footer">
+    <a href="">ivy</a>
+    </div>
+
+  </div>  
+
+  <div class="the-normal-list">
+    <div class="whitecol"></div>
+  </div>
+
+  <div class="the-normal-list">
+    <div class="whitecol"></div>
+  </div>
+  <div class="the-normal-list">
+    <div class="whitecol"></div>
+  </div>
+  <div class="the-normal-list">
+    <div class="whitecol"></div>
+  </div>
+  <div class="the-normal-list">
+    <div class="whitecol"></div>
+  </div>  
+</div>
+
+<div class="rank">
+<p id="e-col-newest"><span class="glyphicon glyphicon-repeat" id="col-icon"></span> <a href="works.php?cata=music">排行榜</a></p>
+
+</div>
+
+</div>
+<?php
+		    break;
+		case "enactment":
+		    
+		    break;
+		case "dubbing":
+		    
+		    break;
+		case "storyboard":
+		    
+		    break;
+	}
+}
+?>
+
 
 <?php include('footer.php'); ?>
